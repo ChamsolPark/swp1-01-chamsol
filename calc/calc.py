834af1b4025501fe1bd1ec148cc56c941e9ad08e
ChamsolPark <osoryo@naver.com>
2020-06-26 14:13:37 +0900

    Issue #4 문자열 입력에 대한 예외 처리 조건문 사용 발생할 수 있는 예외 사항 중 입력값이 정수형이 아닌 문자형일 때를 상정하였습니다. 예외처리에 있어서 입력된 값이 정수형이 아닌 문자형일 때 합과 곱에서 -99999999를 출력하게 했습니다. 이를 위해 조건문에서 정수형이 아닌 경우를 설정했습니다. Close #4

--- calc/calc.py
+++ calc/calc.py
@@ -6,10 +6,13 @@ def application(environ, start_response):
     first_num = d.get('first_num',[''])[0]
     second_num = d.get('second_num',[''])[0]
     sum, mul = -1, -1
-    if '' not in [first_num, second_num]:
+    if first_num.isdigit() and second_num.isdigit():
         first_num, second_num = int(first_num), int(second_num)
         sum = first_num + second_num
         mul = first_num * second_num
+    else:
+        sum= -9999
+        mul= -9999
     response_body = html % {'sum':sum, 'mul':mul}
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
